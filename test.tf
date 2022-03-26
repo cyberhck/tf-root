b10ef264501b88933e27a5983dae3ba863769ac1
Nishchal Gautam <cyberhck@users.noreply.github.com>
2022-03-26 19:08:18 +0700

    Update test.tf

--- test.tf
+++ test.tf
@@ -3,3 +3,8 @@ module "test" {
   repository_description = ""
   repository_name        = "tf-repo-template"
 }
+module "my_new_repo" {
+  source                 = "./modules/template_repository"
+  repository_description = ""
+  repository_name        = "test_repository"
+}
